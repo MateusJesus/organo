0a7a6091fea9eaacf64f31b6cadfe401f1e3a5ea
Mateus de Jesus <mateus191019@gmail.com>
2025-03-15 17:55:56 -0400

    mexi

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,37 +1,45 @@
+import { useState } from "react";
 import { CiTrash } from "react-icons/ci";
 import { GoHeartFill, GoHeart } from "react-icons/go";
-import './Colaborador.css'
+import "./Colaborador.css";
 
 const Colaborador = (props) => {
-    function aoFavoritar() {
-        props.favoritar(props.colaborador.id)
-    }
+  const [imagemSrc, setImagemSrc] = useState(
+    props.colaborador.imagem || "https://cdn-icons-png.flaticon.com/512/5436/5436149.png"
+  );
 
-    const propsFavoritar = {
-        size: 20,
-        onClick: aoFavoritar
-    }
-    return (
-        <div className='colaborador'>
-            <CiTrash className='deletar' onClick={props.aoDeletar} />
-            <div className='cabecalho' style={{ backgroundColor: props.bg }}>
-                <img
-                    src={props.colaborador.imagem}
-                    alt={`imagem do colaborador ${props.colaborador.nome}`}
-                />
-            </div>
-            <div className='rodape'>
-                <h4>{props.colaborador.nome}</h4>
-                <h5>{props.colaborador.cargo}</h5>
-                <div className="favorito">
-                    {props.colaborador.favorito ?
-                        <GoHeartFill {...propsFavoritar} /> :
-                        <GoHeart {...propsFavoritar} />
-                    }
-                </div>
-            </div>
+  function aoFavoritar() {
+    props.favoritar(props.colaborador.id);
+  }
+
+  const propsFavoritar = {
+    size: 20,
+    onClick: aoFavoritar,
+  };
+
+  return (
+    <div className="colaborador">
+      <CiTrash className="deletar" onClick={props.aoDeletar} />
+      <div className="cabecalho" style={{ backgroundColor: props.bg }}>
+        <img
+          src={imagemSrc}
+          alt={`Imagem do colaborador ${props.colaborador.nome}`}
+          onError={() => setImagemSrc("https://cdn-icons-png.flaticon.com/512/5436/5436149.png")}
+        />
+      </div>
+      <div className="rodape">
+        <h4>{props.colaborador.nome}</h4>
+        <h5>{props.colaborador.cargo}</h5>
+        <div className="favorito">
+          {props.colaborador.favorito ? (
+            <GoHeartFill {...propsFavoritar} />
+          ) : (
+            <GoHeart {...propsFavoritar} />
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Colaborador
+export default Colaborador;
