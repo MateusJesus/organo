01f79e944946a87c28d10121115467f2ce0ea414
Mateus de Jesus <mateus191019@gmail.com>
2025-03-16 01:36:24 -0400

    organo

--- src/App.js
+++ src/App.js
@@ -1,96 +1,98 @@
-import { useEffect, useState } from 'react';
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
-import Times from './componentes/Times';
-import { v4 as uuidv4 } from 'uuid'
-import Campo from './componentes/Campo';
+import { useEffect, useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Times from "./componentes/Times";
+import { v4 as uuidv4 } from "uuid";
+import Campo from "./componentes/Campo";
+
+const apiKey = process.env.REACT_APP_API_PASSWORD;
 
 function App() {
-
-  const [times, setTime] = useState([])
-
-  const [colaboradores, setColaboradores] = useState([])
-
-  const [pesquisa, setPesquisa] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:8080/times')
-      .then(resposta => resposta.json())
-      .then(dado => setTime(dado))
-  }, [])
-
-  useEffect(() => {
-    fetch('http://localhost:8080/colaboradores')
-      .then(resposta => resposta.json())
-      .then(dado => setColaboradores(dado))
-  }, [])
-
-  useEffect(() => {
-    fetch('http://localhost:8080/colaboradores')
-    .then((response) => response.json())
-    .then((dados) => {
-      const resultado = dados.filter((colaborador) =>
-        colaborador.cargo.toLowerCase().includes(pesquisa.toLowerCase())
-      );
-      setColaboradores(resultado);
-    })
-    .catch((error) => console.error('Erro na pesquisa:', error));
-  }, [pesquisa])  
-
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    const novoColaborador = { favorito: false, id: uuidv4(), ...colaborador };
   
-    fetch('http://localhost:8080/colaboradores', {
-      method: 'POST',
+  const [loading, setLoading] = useState(true);
+  
+  const [times, setTime] = useState([]);
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const [pesquisa, setPesquisa] = useState([]);
+
+  useEffect(() => {
+    fetch("https://organo-api-fm95.onrender.com/api", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
+        "X-API-PASSWORD": apiKey,
       },
-      body: JSON.stringify(novoColaborador),
+    })
+      .then((resposta) => resposta.json())
+      .then((dado) => {
+        setColaboradores(dado.colaboradores);
+        setTime(dado.times);
+        setLoading(false)
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://organo-api-fm95.onrender.com/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-PASSWORD": apiKey,
+      },
     })
       .then((response) => response.json())
       .then((dados) => {
-        setColaboradores((prevColaboradores) => [...prevColaboradores, dados]);
+        const resultado = dados.colaboradores.filter((colaborador) => {
+          const pesquisaString = typeof pesquisa === "string" ? pesquisa : "";
+          return colaborador.nome
+            .toLowerCase()
+            .includes(pesquisaString.toLowerCase());
+        });
+        setColaboradores(resultado);
       })
-      .catch((error) => console.error('Erro ao adicionar colaborador:', error));
+      .catch((error) => console.error("Erro na pesquisa:", error));
+  }, [pesquisa]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    const novoColaborador = { favorito: false, id: uuidv4(), ...colaborador };
+    setColaboradores((prevColaboradores) => [
+      ...prevColaboradores,
+      novoColaborador,
+    ]);
   };
 
   function cadastrarTime(time) {
     const novoTime = { id: uuidv4(), ...time };
-  
-    fetch('http://localhost:8080/times', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(novoTime),
-    })
-      .then((response) => response.json())
-      .then((dados) => {
-        setTime((prevTimes) => [...prevTimes, dados]);
-      })
-      .catch((error) => console.error('Erro ao cadastrar time:', error));
+    setTime((prevTimes) => [...prevTimes, novoTime]);
   }
 
   function deletarColaborador(id) {
-    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
+    setColaboradores(
+      colaboradores.filter((colaborador) => colaborador.id !== id)
+    );
   }
 
   function mudarCorTime(cor, id) {
-    setTime(times.map(time => {
-      if (time.id === id) {
-        time.cor = cor
-      }
-      return time
-    }))
+    setTime(
+      times.map((time) => {
+        if (time.id === id) {
+          time.cor = cor;
+        }
+        return time;
+      })
+    );
   }
 
   function aoFavoritar(id) {
-    setColaboradores(colaboradores.map(colaborador => {
-      if (colaborador.id === id) {
-        colaborador.favorito = !colaborador.favorito
-      }
-      return colaborador
-    }))
+    setColaboradores(
+      colaboradores.map((colaborador) => {
+        if (colaborador.id === id) {
+          colaborador.favorito = !colaborador.favorito;
+        }
+        return colaborador;
+      })
+    );
   }
 
   return (
@@ -98,27 +100,32 @@ function App() {
       <Banner />
       <Formulario
         cadastrarTime={cadastrarTime}
-        times={times.map(time => ({ id: time.id, nome: time.nome }))}
-        aoColaboradorCadastrado={
-          colaborador => aoNovoColaboradorAdicionado(colaborador)
+        times={times.map((time) => ({ id: time.id, nome: time.nome }))}
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
         }
       />
       <Campo
-        type='text'
+        type="text"
         obrigatorio={true}
         label="Pesquisar colaborador"
         placeholder="Digite o nome do colaborador"
-        aoAlterado={valor => setPesquisa(valor)}
+        aoAlterado={(valor) => setPesquisa(valor)}
       />
-      {times.map((time, index) =>
-        <Times
-          favoritar={aoFavoritar}
-          key={time.nome + index}
-          mudarCor={mudarCorTime}
-          aoDeletar={deletarColaborador}
-          time={time}
-          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.id)}
-        />)}
+      {loading && <h1>Carregando colaboradores...</h1>}
+      {!loading &&
+        times.map((time, index) => (
+          <Times
+            favoritar={aoFavoritar}
+            key={time.nome + index}
+            mudarCor={mudarCorTime}
+            aoDeletar={deletarColaborador}
+            time={time}
+            colaboradores={colaboradores.filter(
+              (colaborador) => colaborador.time === time.id
+            )}
+          />
+        ))}
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,99 +1,99 @@
-import { useState } from 'react'
-import Botao from '../Botao'
-import Campo from '../Campo'
-import ListaSuspensa from '../ListaSuspensa'
-import './Formulario.css'
+import { useState } from "react";
+import Botao from "../Botao";
+import Campo from "../Campo";
+import ListaSuspensa from "../ListaSuspensa";
+import "./Formulario.css";
 
 const Formulario = (props) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+  const [nomeTime, setNomeTime] = useState("");
+  const [corTime, setCorTime] = useState("");
 
-    const [nome, setNome] = useState('')
-    const [cargo, setCargo] = useState('')
-    const [imagem, setImagem] = useState('')
-    const [time, setTime] = useState('')
-    const [nomeTime, setNomeTime] = useState('')
-    const [corTime, setCorTime] = useState('')
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
+    setNome("");
+    setCargo("");
+    setImagem("");
+    setTime("");
+  };
 
-    const aoSalvar = (evento) => {
-        evento.preventDefault()
-        props.aoColaboradorCadastrado({
-            nome,
-            cargo,
-            imagem,
-            time
-        })
-        setNome('')
-        setCargo('')
-        setImagem('')
-        setTime('')
-    }
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <Campo
+          type="text"
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <Campo
+          type="text"
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <Campo
+          type="text"
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={props.times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
+        <Botao>Criar Card</Botao>
+      </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          props.cadastrarTime({
+            nome: nomeTime,
+            cor: corTime === "" ? "#ffaabb" : corTime,
+          });
+          setNomeTime("");
+          setCorTime("");
+        }}
+      >
+        <h2>Preencha os dados para criar um novo time</h2>
+        <Campo
+          type="text"
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o nome do seu time"
+          valor={nomeTime}
+          aoAlterado={(valor) => setNomeTime(valor)}
+        />
+        <Campo
+          type="color"
+          obrigatorio={true}
+          label="Cor do time"
+          placeholder="Digite sua cor"
+          valor={corTime === "" ? "#ffaabb" : corTime}
+          aoAlterado={(valor) => setCorTime(valor)}
+        />
+        <Botao>Criar Time</Botao>
+      </form>
+    </section>
+  );
+};
 
-    return (
-        <section className="formulario">
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <Campo
-                    type='text'
-                    obrigatorio={true}
-                    label="Nome"
-                    placeholder="Digite seu nome"
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                />
-                <Campo
-                    type='text'
-                    obrigatorio={true}
-                    label="Cargo"
-                    placeholder="Digite seu cargo"
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                />
-                <Campo
-                    type='text'
-                    label="Imagem"
-                    placeholder="Digite o endereço da imagem"
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}
-                />
-                <ListaSuspensa
-                    obrigatorio={true}
-                    label="Time"
-                    itens={props.times}
-                    valor={time}
-                    aoAlterado={valor => setTime(valor)}
-                />
-                <Botao>
-                    Criar Card
-                </Botao>
-            </form>
-            <form
-                onSubmit={(event) => {
-                    event.preventDefault()
-                    props.cadastrarTime({ nome: nomeTime, cor: corTime === '' ? "#ffaabb" : corTime })
-                }}
-            >
-                <h2>Preencha os dados para criar um novo time</h2>
-                <Campo
-                    type='text'
-                    obrigatorio={true}
-                    label="Nome"
-                    placeholder="Digite o nome do seu time"
-                    valor={nomeTime}
-                    aoAlterado={valor => setNomeTime(valor)}
-                />
-                <Campo
-                    type='color'
-                    obrigatorio={true}
-                    label="Cor do time"
-                    placeholder="Digite sua cor"
-                    valor={corTime === '' ? "#ffaabb" : corTime}
-                    aoAlterado={valor => setCorTime(valor)}
-                />
-                <Botao>
-                    Criar Time
-                </Botao>
-            </form>
-        </section>
-    )
-}
-
-export default Formulario
+export default Formulario;
